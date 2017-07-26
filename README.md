0e89a683b340b5966bced15c3766fae8dc96ead8
d-smith <doug.smith.mail@gmail.com>
2017-07-26 12:23:29 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+hello-beanstalk
+
+Simple hello world web application for working through an
+Elastic Beanstalk deploy.
---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,26 @@
+package hello;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
